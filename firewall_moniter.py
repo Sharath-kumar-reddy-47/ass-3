5f835c681c9806ca3d273c2737c9b5c55c85cec2
Sharath-kumar-reddy-47 <gandhamsharath@47gmail.com>
2023-11-02 15:54:33 +0530

    uhb

--- firewall_moniter.py
+++ firewall_moniter.py
@@ -3,20 +3,21 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import packet, ethernet, ether_types, ipv4
+from ryu.ofproto.ofproto_v1_3_parser import OFPActionSetField, OFPMatch
+from ryu.lib import mac
 
 class FirewallMonitor(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(FirewallMonitor, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
         self.firewall_rules = [
-            {"src_ip": "10.0.0.2", "dst_ip": "10.0.0.3"},
-            {"src_ip": "10.0.0.1", "dst_ip": "10.0.0.4"}
+            {"src_ip": "10.0.0.1", "dst_ip": "10.0.0.4"},  # H1 to H4
+            {"src_ip": "10.0.0.3", "dst_ip": "10.0.0.5"},  # H3 to H5
         ]
-        self.packet_count = 0
+        self.packet_count = 0  # Packet count from H3 on S1
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -24,41 +25,38 @@ class FirewallMonitor(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Install firewall rules on each switch
+        # Install firewall rules
         for rule in self.firewall_rules:
-            match = parser.OFPMatch(
-                eth_type=ether_types.ETH_TYPE_IP,
-                ipv4_src=rule["src_ip"],
-                ipv4_dst=rule["dst_ip"]
-            )
-            actions = []  # Drop the packet
+            match = OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
+                             ipv4_src=rule["src_ip"],
+                             ipv4_dst=rule["dst_ip"])
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
             self.add_flow(datapath, 1, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         if eth.ethertype == ether_types.ETH_TYPE_IP:
-            ip_pkt = pkt.get_protocol(packet.ipv4.ipv4)
-            if ip_pkt.src == "10.0.0.3":
-                # Count packets from host H3 on switch S1
-                self.packet_count += 1
+            ip = pkt.get_protocol(ipv4.ipv4)
 
-    def add_flow(self, datapath, priority, match, actions):
+            # Count packets from H3 on switch S1
+            if datapath.id == 1 and ip.src == "10.0.0.3":
+                self.packet_count += 1
+                self.logger.info("Packet count from H3 on S1: %d", self.packet_count)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(
-            datapath=datapath,
-            priority=priority,
-            match=match,
-            instructions=inst
-        )
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id, priority=priority, match=match, instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
         datapath.send_msg(mod)
